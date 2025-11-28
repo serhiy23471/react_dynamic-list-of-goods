c06c83892c1a2e21b53f9932c10392f38909e23d
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-11-28 17:21:47 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,84 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
-import { GoodsList } from './GoodsList';
+import GoodsList from './GoodsList';
+import { Good } from './types/Good';
+import { getAll, get5First, getRedGoods } from './api/goods';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+enum GoodsFilter {
+  All = 'All',
+  FirstFive = 'FirstFive',
+  RedOnly = 'RedOnly',
+  Default = All,
+}
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [filterMethod, setFilterMethod] = useState<GoodsFilter | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (!filterMethod) {
+      return;
+    }
+    setIsLoading(true);
+    setErrorMsg('');
+    let res: Promise<Good[]>;
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+    switch (filterMethod) {
+      case GoodsFilter.All:
+        res = getAll();
+        break;
+      case GoodsFilter.FirstFive:
+        res = get5First();
+        break;
+      case GoodsFilter.RedOnly:
+        res = getRedGoods();
+        break;
+    }
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+    res
+      .then(setGoods)
+      .catch(() => setErrorMsg('Sth went wrong. Try later.'))
+      .finally(() => setIsLoading(false));
+  }, [filterMethod]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleFilterSelection = useCallback(
+    (method: GoodsFilter = GoodsFilter.All) => {
+      setFilterMethod(method);
+    },
+    [],
+  );
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleFilterSelection(GoodsFilter.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleFilterSelection(GoodsFilter.FirstFive)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleFilterSelection(GoodsFilter.RedOnly)}
+      >
+        Load red goods
+      </button>
+
+      {isLoading ? <p>Loading...</p> : <GoodsList goods={goods} />}
+
+      {errorMsg && <h4>{errorMsg}</h4>}
+    </div>
+  );
+};
